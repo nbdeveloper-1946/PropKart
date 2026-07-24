0c4ae54379ded3e8c47718c1c5a3f8c021a70343
JAYU0812 <jay13soni@gmail.com>
2026-07-24 14:48:11 +0530

    feat(config): change public share base url to propkart domain, keep nb-listings as backup

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,3 +1,4 @@
 class AppConfig {
-  static const String publicShareBaseUrl = "https://nb-listings.vercel.app/share";
+  static const String publicShareBaseUrl = "https://propkart.vercel.app/share";
+  static const String backupShareBaseUrl = "https://nb-listings.vercel.app/share";
 }
